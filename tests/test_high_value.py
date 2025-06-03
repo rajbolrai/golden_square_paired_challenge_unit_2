e374d7a08859d193d08fd26f63bdcbf99bb9de7b
Chris Mecrow <mecrow@live.co.uk>
2025-06-03 16:17:14 +0100

    Completed tests for high_value

--- tests/test_high_value.py
+++ tests/test_high_value.py
@@ -1,2 +1,48 @@
 from lib.high_value import *
 
+"""
+Try to make an object with the class, if not found
+then should return an error
+"""
+def test_giving_a_high_second_value_output_second_value():
+    obj_high_value = HighValue(0,1)
+    result = obj_high_value.get_highest()
+    assert result == "Second value is higher"
+
+def test_giving_a_high_first_value_output_first_value():
+    obj_high_value = HighValue(1,0)
+    result = obj_high_value.get_highest()
+    assert result == "First value is higher"
+
+def test_giving_same_value_output_same_value():
+    obj_high_value = HighValue(1,1)
+    result = obj_high_value.get_highest()
+    assert result == "Values are equal"
+
+def test_invalid_first_selection_output_nothing():
+    obj_high_value = HighValue(1,1)
+    original_value = obj_high_value.value_first
+    obj_high_value.add(1, " ")
+    equal = original_value == obj_high_value.value_first
+    assert equal == True
+
+def test_invalid_second_selection_output_nothing():
+    obj_high_value = HighValue(1,1)
+    original_value = obj_high_value.value_second
+    obj_high_value.add(1, " ")
+    equal = original_value == obj_high_value.value_second
+    assert equal == True
+
+def test_valid_first_selection_output_increased_first_value():
+    obj_high_value = HighValue(1,1)
+    original_value = obj_high_value.value_first
+    obj_high_value.add(5,"first")
+    equal = original_value != obj_high_value.value_first
+    assert equal == True
+
+def test_valid_second_selection_output_increased_second_value():
+    obj_high_value = HighValue(1,1)
+    original_value = obj_high_value.value_second
+    obj_high_value.add(5,"second")
+    equal = original_value != obj_high_value.value_second
+    assert equal == True
